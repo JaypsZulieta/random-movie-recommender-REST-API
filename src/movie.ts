85df88495aef2319d3c6a364f2e5d96fa9614b2b
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-04 05:50:34 +0800

    initial commit

---
+++ src/movie.ts
@@ -0,0 +1,65 @@
+class Movie {
+  constructor(
+    readonly id: number,
+    readonly title: string,
+    readonly overview: string,
+    readonly releaseDate: Date,
+    readonly backDropURL: string | null,
+    readonly posterURL: string | null
+  ) {
+    this.backDropURL = !backDropURL ? null : `https://image.tmdb.org/t/p/original${backDropURL}`;
+    this.posterURL = !posterURL ? null : `https://image.tmdb.org/t/p/original${posterURL}`;
+  }
+}
+
+class MovieBuilder {
+  private idToSet = 0;
+  private titleToSet = "John Smith - The Movie";
+  private overviewToSet = "The life story of the legendary John Smith.";
+  private releaseDateToSet = new Date("2003-12-20");
+  private backDropURLToSet: string | null = null;
+  private posterURLToSet: string | null = null;
+
+  id(id: number): MovieBuilder {
+    this.idToSet = id;
+    return this;
+  }
+
+  title(title: string): MovieBuilder {
+    this.titleToSet = title;
+    return this;
+  }
+
+  overview(overview: string): MovieBuilder {
+    this.overviewToSet = overview;
+    return this;
+  }
+
+  releaseDate(date: Date): MovieBuilder {
+    this.releaseDateToSet = date;
+    return this;
+  }
+
+  backDropURL(backDropURL: string | null): MovieBuilder {
+    this.backDropURLToSet = backDropURL;
+    return this;
+  }
+
+  posterURL(posterURL: string | null): MovieBuilder {
+    this.posterURLToSet = posterURL;
+    return this;
+  }
+
+  build(): Movie {
+    return new Movie(
+      this.idToSet,
+      this.titleToSet,
+      this.overviewToSet,
+      this.releaseDateToSet,
+      this.backDropURLToSet,
+      this.posterURLToSet
+    );
+  }
+}
+
+export { type Movie, MovieBuilder };
